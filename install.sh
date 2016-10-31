fd5b7dbef8dfb70a397b8ffd4b3c617c40bcc0e8
Jagan Teki <jagan@openedev.com>
2016-10-31 17:15:43 +0530

    Fixes
    Signed-off-by: Jagan Teki <jagan@openedev.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
-jcode
+alias jcode='cd /home/jagan/JSpace/code'
 
+jcode
 sudo echo 'install apt-get'
-sudo apt-get update && apt-get upgrade && apt-get update && sudo apt-get minicom install vim flex bison libsdl2-dev libssl-dev git-email
+sudo apt-get update && sudo apt-get upgrade && sudo apt-get update && sudo apt-get minicom install vim flex bison libsdl2-dev libssl-dev git-email
 
 sudo echo 'copy SSH keys'
 sudo chmod 600 ~/.ssh/id_rsa
@@ -9,14 +10,14 @@ sudo chmod 644 ~/.ssh/id_rsa.pub
 
 sudo echo 'clone u-boot-spi'
 sudo git clone git://git.denx.de/u-boot-spi.git
-cd u-boot-spi
-git remote add spi-write ssh://gu-spi@git.denx.de/u-boot-spi.git
-git remote add tom git://git.denx.de/u-boot.git
-git remote add as ssh://git@bitbucket.amarulasolutions.com:7999/en/u-boot-engicam.git
+sudo cd u-boot-spi
+sudo git remote add spi-write ssh://gu-spi@git.denx.de/u-boot-spi.git
+sudo git remote add tom git://git.denx.de/u-boot.git
+sudo git remote add as ssh://git@bitbucket.amarulasolutions.com:7999/en/u-boot-engicam.git
 
 sudo echo 'install buildman'
 HOME=/home/jagan/JSpace/tools
-cp /home/jagan/JSpace/tools/buildman .buildman
+sudo cp /home/jagan/JSpace/tools/buildman .buildman
 ./tools/buildman/buildman --list-tool-chains
 ./tools/buildman/buildman --fetch-arch
 ./tools/buildman/buildman --list-tool-chains
@@ -24,9 +25,9 @@ cp /home/jagan/JSpace/tools/buildman .buildman
 jcode
 sudo echo 'install dtc'
 sudo git clone git://git.kernel.org/pub/scm/utils/dtc/dtc.git
-cd dtc
+sudo cd dtc
 sudo make
-mkdir /home/jagan/JSpace/tools/dtc
+sudo mkdir /home/jagan/JSpace/tools/dtc
 sudo make install --prefix=/home/jagan/JSpace/tools/dtc
 
 jcode
